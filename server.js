c07839acb9c705c76c8c0fabd752d4621efbfbd5
Lauren Templeton <etothel0514@gmail.com>
2016-11-07 14:56:33 -0500

    added routes folder, set up server

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//const dotEnv          = require('dotenv').config({silent: true});
+
+const express         = require('express');
+const logger          = require('morgan');
+const bodyParser      = require('body-parser');
+const pgPromise       = require('pg-promise');
+const path            = require('path');
+// tests to see if we have NODE_ENV in our environment
+// only load the dotenv if we need it
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config && true;
+
+const app = express();
+const port = process.argv[2] || process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+app.use(logger(isDev ? 'dev' : 'common'));
+
+// Set static file root folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//  going to accept json
+app.use(bodyParser.json());
+
+// generic error handler
+app.use((err, req, res, next) => {
+  console.error(err, next);
+  res.status(500).send('Something broke!');
+});
+
+
